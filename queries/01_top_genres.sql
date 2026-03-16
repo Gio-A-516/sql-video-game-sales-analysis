8b018ebbfe794e9bdf1cce0983378c777c1a1cbe
Gio-A-516 <gioarcari3@gmail.com>
2026-03-16 16:08:48 -0500

    Add comment to top genres SQL query
    Added comments to clarify the purpose of the query.

--- queries/01_top_genres.sql
+++ queries/01_top_genres.sql
@@ -1,1 +1,7 @@
+-- Identify the top genres by total global sales
 
+select genre, sum(global_sales) as total_global_sales
+from video_game_sales
+group by genre
+order by sum(global_sales) desc
+limit 5;
